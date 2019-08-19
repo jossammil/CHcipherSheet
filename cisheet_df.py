fbbe3c2272c4118d57c539615ded97a4421364e4
jossammil <jossammil@gmail.com>
2019-08-18 22:20:06 -0500

    Adding the dataframe creation

---
+++ cisheet_df.py
@@ -0,0 +1,16 @@
+
+"""
+@author: jossammil
+"""
+
+import pandas as pd
+
+#%%
+
+newpd = pd.DataFrame({'ring':ring_col, 
+                      'rotor': rotors_month,
+                      'pluglist': pluglist_month,
+                      'kengruppen': msg_col
+                      })
+
+
